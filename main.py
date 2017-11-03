667e8592c69927f8e3619df002a4fb1b53ad7744
Xinhe Ren <jim.x.ren@berkeley.edu>
2017-11-02 18:28:17 -0700

    Main script now annotate static images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #/home/jren/env/bin/python
 from __future__ import unicode_literals
 
+import cv2
 import youtube_dl
 from urlparse import parse_qs
 
@@ -12,7 +13,9 @@ youtube_ids = {
 }
 
 streams = {
-    'rush_hour': '../uds_video_demo/rush_hour.mp4'
+    'rush_hour': '../uds_video_demo/rush_hour.mp4',
+    'alberta_cam_day_demo': '../uds_video_demo/alberta_cam_original_2017-10-27_10-00-30.mp4',
+    'alberta_cam_night_demo': '../uds_video_demo/alberta_cam_original_2017-10-26_21-00-39.mp4',
 }
 
 class M3U8Logger(object):
@@ -48,13 +51,11 @@ def getYoutubeStreamURL(stream_name, youtube_video_id):
         ydl.download(['https://www.youtube.com/watch?v=%s' % youtube_video_id])
         streams[stream_name] = m3u8.url
 
-def main():
-    getYoutubeStreamURL('alberta_cam', youtube_ids['alberta_cam'])
-    # ssd_detector = SSDDetector('ssd', 'VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt', streams['alberta_cam'])
-    # ssd_detector.displayAnnotatedFrames()
-    # ssd_detector.saveAnnotatedFrames('/nfs/diskstation/jren/test/alberta_cam', 60)
-    no_detector = AbstractDetector('original', streams['alberta_cam'])
+def saveAlbertaCam():
+    ssd_detector.saveAnnotatedFrames('alberta_cam_night_demo', root_path='../uds_video_demo/annotated')
 
+    no_detector = AbstractDetector('original', streams['alberta_cam'])
+    getYoutubeStreamURL('alberta_cam', youtube_ids['alberta_cam'])
     dir_size_maxed = False
     while not dir_size_maxed:
         getYoutubeStreamURL('alberta_cam', youtube_ids['alberta_cam'])
@@ -63,6 +64,14 @@ def main():
                                                          root_path='/nfs/diskstation/jren/alberta_cam/',
                                                          segment_length=60,
                                                          dir_size_limit=1e12)
+
+def main():
+    # ssd_detector = SSDDetector('ssd', 'VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt', streams['alberta_cam_night_demo'])
+    # ssd_detector.displayAnnotatedFrames()
+
+    img = cv2.imread('../uds_video_demo/alberta_nobox.png', cv2.IMREAD_COLOR)
+    ssd_detector = SSDDetector('ssd', 'VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt')
+    print ssd_detector.process_image(img)
 
 if __name__ == "__main__":
     main()
